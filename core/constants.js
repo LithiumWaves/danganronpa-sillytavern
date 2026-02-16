e80dd3f9ae19df5148aba08d074d122b568e61ab
LithiumWaves <pressstart132@gmail.com>
2026-02-15 23:57:19 -0300

    Make settings panel scrollable on mobile

--- core/constants.js
+++ core/constants.js
@@ -22,6 +22,13 @@ export const MONOCOIN_REWARDS = {
     trustMaxed: 50,
 };
 
+export const XP_REWARDS = {
+    truthBullet: 8,
+    socialRankUp: 10,
+    trustMaxed: 25,
+    giftGiven: 5,
+};
+
 export const SOCIAL_REGEX = /V3C\|\s*SOCIAL:\s*([^\n\r]+)/g;
 export const SOCIAL_UP_REGEX = /V3C\|\s*SOCIAL_UP:\s*([^\n\r]+)/g;
 export const SOCIAL_DOWN_REGEX = /V3C\|\s*SOCIAL_DOWN:\s*([^\n\r]+)/g;
